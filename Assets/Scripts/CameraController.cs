df67b9aa88b935bd911240e915988296c4072ff4
ragnor <ozanemirhan@gmail.com>
2021-04-24 19:14:33 +0300

    basic camera controls added.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float MinimumX;
+    public float MaximumX;
 
-    // Update is called once per frame
+    public float MinimumY;
+    public float MaximumY;
+
+    public float MinimumZ;
+    public float MaximumZ;
+
+    public float ScreenDelta;
+
+    public float CameraMoveSpeed = 1.0f;
+    public float CameraZoomSpeed = 1.0f;
+
     void Update()
     {
-        
+        GetUserInput(Time.deltaTime);
+
+    }
+
+    void GetUserInput(float dt)
+    {
+        var translation = Vector3.zero;
+
+        if (Input.mousePosition.x >= Screen.width - ScreenDelta)
+        {
+            // Move the camera
+            translation += transform.right * CameraMoveSpeed * dt;
+        }
+        if (Input.mousePosition.x <= 0 + ScreenDelta)
+        {
+            // Move the camera
+            translation -= transform.right * CameraMoveSpeed * dt;
+        }
+        if (Input.mousePosition.y >= Screen.height - ScreenDelta)
+        {
+            // Move the camera
+            translation += transform.up * CameraMoveSpeed * dt;
+        }
+        if (Input.mousePosition.y <= 0 + ScreenDelta)
+        {
+            // Move the camera
+            translation -= transform.up * CameraMoveSpeed * dt;
+        }
+
+
+        // Zoom in or out
+        var zoomDelta = Input.GetAxis("Mouse ScrollWheel") * CameraZoomSpeed * dt;
+        if (zoomDelta != 0)
+        {
+            translation += transform.forward * zoomDelta;
+        }
+        transform.position += translation;
+        translation = transform.position;
+
+        translation.x = Mathf.Clamp(translation.x, MinimumX, MaximumX);
+        translation.y = Mathf.Clamp(translation.y, MinimumY, MaximumY);
+        translation.z = Mathf.Clamp(translation.z, MinimumZ, MaximumZ);
+
+        transform.position = translation;
     }
 }
