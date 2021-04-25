2e4e559a2258611f9fa625f6533cdb6430d6e660
mestan1 <serhan477@gmail.com>
2021-04-25 09:42:35 +0100

    Main UI and Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -13,59 +14,145 @@ public class CameraController : MonoBehaviour
     public float MinimumZ;
     public float MaximumZ;
 
-    public float ScreenDelta;
+    public float ScreenDeltaX;
+    public float ScreenDeltaY;
+
 
     public float CameraMoveSpeed = 1.0f;
     public float CameraZoomSpeed = 1.0f;
 
+    bool pan, goingRight, goingUp;
+    Vector3 startPos, translation;
+    private void Start()
+    {
+        //ResolutionAdjust();
+    }
     void Update()
     {
-        if (Input.GetMouseButton(1))
-            GetUserInput(Time.deltaTime);
+        translation = Vector3.zero;
+        if (Input.GetMouseButtonDown(1))
+        {
+            pan = true;
+            startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(1)) pan = false;
+
+        if (pan) translation = ScrollMouse(startPos, Time.deltaTime);
+        translation = ScrollEdge(Time.deltaTime);
+        translation = Zoom(Time.deltaTime);
 
     }
 
-    void GetUserInput(float dt)
+    private void LateUpdate()
     {
-        Vector3 translation = Vector3.zero;
+        if (MouseScreenCheck())
+        {
+            transform.position += translation;
 
-        if (Input.mousePosition.x >= Screen.width - ScreenDelta)
+            translation = transform.position;
+
+            translation.x = Mathf.Clamp(translation.x, MinimumX, MaximumX);
+            translation.y = Mathf.Clamp(translation.y, MinimumY, MaximumY);
+            translation.z = Mathf.Clamp(translation.z, MinimumZ, MaximumZ);
+
+            transform.position = translation;
+
+        }
+    }
+
+    Vector3 ScrollMouse(Vector3 startPos, float dt)
+    {
+        var mousePosition = Input.mousePosition;
+        if (mousePosition.x >= startPos.x)
+        {
+            translation += transform.right * CameraMoveSpeed * dt;
+            goingRight = true;
+        }
+        if (mousePosition.x <= startPos.x)
+        {
+            // Move the camera
+            translation -= transform.right * CameraMoveSpeed * dt;
+            goingRight = false;
+        }
+        if (mousePosition.y >= startPos.y)
+        {
+            // Move the camera
+            translation += transform.up * CameraMoveSpeed * dt;
+            goingUp = true;
+        }
+        if (mousePosition.y <= startPos.y)
+        {
+            // Move the camera
+            translation -= transform.up * CameraMoveSpeed * dt;
+            goingUp = false;
+        }
+
+
+        return translation;
+    }
+
+    Vector3 ScrollEdge(float dt)
+    {
+        if (Input.mousePosition.x >= Screen.width - ScreenDeltaX)
         {
             // Move the camera
             translation += transform.right * CameraMoveSpeed * dt;
         }
-        if (Input.mousePosition.x <= 0 + ScreenDelta)
+        if (Input.mousePosition.x <= 0 + ScreenDeltaX)
         {
             // Move the camera
             translation -= transform.right * CameraMoveSpeed * dt;
         }
-        if (Input.mousePosition.y >= Screen.height - ScreenDelta)
+        if (Input.mousePosition.y >= Screen.height - ScreenDeltaY)
         {
             // Move the camera
             translation += transform.up * CameraMoveSpeed * dt;
         }
-        if (Input.mousePosition.y <= 0 + ScreenDelta)
+        if (Input.mousePosition.y <= 0 + ScreenDeltaY)
         {
             // Move the camera
             translation -= transform.up * CameraMoveSpeed * dt;
         }
+        return translation;
+    }
 
-
+    Vector3 Zoom(float dt)
+    {
         // Zoom in or out
         var zoomDelta = Input.GetAxis("Mouse ScrollWheel") * CameraZoomSpeed * dt;
         if (zoomDelta != 0)
         {
             translation += transform.forward * zoomDelta;
         }
-        transform.position += translation;
+        return translation;
+    }
 
-        translation = transform.position;
+    void ResolutionAdjust()
+    {
+        float height = Screen.width;
+        float width = Screen.height;
 
-        translation.x = Mathf.Clamp(translation.x, MinimumX, MaximumX);
-        translation.y = Mathf.Clamp(translation.y, MinimumY, MaximumY);
-        translation.z = Mathf.Clamp(translation.z, MinimumZ, MaximumZ);
-
-        transform.position = translation;
+        ScreenDeltaX = ScreenDeltaX * width / 100;
+        ScreenDeltaY = ScreenDeltaY * height / 100;
 
     }
-}
+    bool MouseScreenCheck()
+    {
+#if UNITY_EDITOR
+        if (Input.mousePosition.y <= 0 || Input.mousePosition.x <= 0 || Input.mousePosition.x >= Handles.GetMainGameViewSize().x - 1 || Input.mousePosition.y >= Handles.GetMainGameViewSize().y - 1)
+        {
+            return false;
+        }
+#else
+        if (Input.mousePosition.x <= 0 || Input.mousePosition.y <= 0 || Input.mousePosition.x >= Screen.width - 1 || Input.mousePosition.y >= Screen.height - 1) {
+        return false;
+        }
+#endif
+        else
+        {
+            return true;
+        }
+    }
+
+
+    }
