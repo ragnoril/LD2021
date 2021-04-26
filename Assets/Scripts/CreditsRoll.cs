b664e7e25ad9386c3b2bfd349c28f9ef2c9bf5b3
ragnor <ozanemirhan@gmail.com>
2021-04-26 21:00:36 +0300

    Main menu scene added.

---
+++ Assets/Scripts/CreditsRoll.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsRoll : MonoBehaviour
+{
+    public RectTransform RollTextTransform;
+    private float _startPos;
+    private float _resetPos;
+    public float MoveSpeed;
+
+    public bool IsPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = -RollTextTransform.sizeDelta.y;
+        _resetPos = RollTextTransform.sizeDelta.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            IsPaused = true;
+        else
+            IsPaused = false;
+
+        if (!IsPaused)
+        {
+            Vector3 pos = RollTextTransform.localPosition;
+            pos.y += MoveSpeed;
+
+            if (pos.y > _resetPos)
+            {
+                pos.y = _startPos;
+            }
+
+            RollTextTransform.localPosition = pos;
+        }
+    }
+
+    public void ToggleCredits()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+}
